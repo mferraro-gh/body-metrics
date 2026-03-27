0eb28ca0d36aaee09cb32d7f793a9e6d4c1d4581
Marco Ferraro <mantofer2000@gmail.com>
2026-03-26 23:34:27 -0600

    adding docker file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const backendUrl = process.env.BACKEND_URL || 'http://localhost:8000'
+
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: '0.0.0.0',
     proxy: {
-      '/api': 'http://localhost:8000',
-      '/data': 'http://localhost:8000'
+      '/api': backendUrl,
+      '/data': backendUrl
     }
   }
 })
